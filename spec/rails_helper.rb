005509ba7dc0919de92924b443f9633ec0b34d0d
hatanatsu1993 <086.yga4jjguaf5@gmail.com>
2025-02-21 00:13:45 +0900

    結合テストの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,6 @@ end
 I18n.locale = "en"
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
@@ -45,7 +44,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include SignInSupport
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
@@ -70,4 +69,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  
 end
